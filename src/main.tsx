e6032311f89b24516f0550b86c411485071e701a
Maires Souza <trabalho.maires@gmail.com>
2023-09-29 16:19:35 -0300

    initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
+// import { Provider } from 'react-redux';
 import App from './App';
 
 ReactDOM
   .createRoot(document.getElementById('root') as HTMLElement)
   .render(
     <BrowserRouter>
+      {/* <Provider store={ store }> */}
       <App />
+      {/* </Provider> */}
     </BrowserRouter>,
   );
