bf85f8d3af35475538cd33a077bcd99d1dad46c5
Maires Souza <trabalho.maires@gmail.com>
2023-09-30 09:36:37 -0300

    style: login page

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,8 +1,13 @@
 import { useSelector } from 'react-redux';
 import { UserWallet } from '../../types';
-import { Form } from './LoginStyle';
+import { Form, Contanier, Logo, LogoContainer, Name } from './LoginStyle';
 import InputsForm from '../../components/InputsForm';
 import LogiForm from '../../utils/formHandlers';
+import Trybe from '../../assets/Trybe.svg';
+import Wallet from '../../assets/Wallet.svg';
+import Rectangle1 from '../../assets/Rectangle1.svg';
+import Rectangle2 from '../../assets/Rectangle2.svg';
+import emoji_money_with_wings_ from '../../assets/emoji_money_with_wings_.svg';
 
 function Login() {
   const regEx = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
@@ -13,14 +18,27 @@ function Login() {
     handlePasswordChange, handleNavigate } = LogiForm();
 
   return (
-    <Form>
-      <InputsForm
-        handleEmailChange={ handleEmailChange }
-        handlePasswordChange={ handlePasswordChange }
-        handleNavigate={ handleNavigate }
-        isValidForm={ isValidForm && password.length >= 6 }
-      />
-    </Form>
+    <Contanier>
+      <LogoContainer>
+        <Logo>
+          <img src={ Rectangle1 } alt="rectangle icon 1" />
+          <img src={ Rectangle2 } alt="rectangle icon 2" />
+          <img src={ emoji_money_with_wings_ } alt="emoji money with wings" />
+        </Logo>
+        <Name>
+          <img src={ Trybe } alt="trybe icon" />
+          <img src={ Wallet } alt="wallet icon" />
+        </Name>
+      </LogoContainer>
+      <Form>
+        <InputsForm
+          handleEmailChange={ handleEmailChange }
+          handlePasswordChange={ handlePasswordChange }
+          handleNavigate={ handleNavigate }
+          isValidForm={ isValidForm && password.length >= 6 }
+        />
+      </Form>
+    </Contanier>
   );
 }
 
--- src/pages/login/LoginStyle.tsx
+++ src/pages/login/LoginStyle.tsx
@@ -1,11 +1,114 @@
 import styled from 'styled-components';
 
+export const Contanier = styled.div`
+width: 32.813rem;
+height: 22.25rem;
+border-radius: 0.625rem;
+background: #FFF;
+box-shadow: -0.25rem 0.563rem 0.813rem, 0 rgba(3, 107, 82, 0.30);
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+`;
+
 export const Form = styled.form`
 display: flex;
 flex-direction: column;
 flex-shrink: 0;
-border: 1px solid red;
 border-radius: 0.3125rem;
 width: 20.625rem;
 height: 8.75rem;
+
+button {
+border-radius: 0.313rem;
+border: 0.063rem solid #003BE5;
+background: #003BE5;
+
+width: 20.625rem;
+height: 2.5rem;
+flex-shrink: 0;
+
+color: #FFF;
+text-align: center;
+font-family: Epilogue;
+font-size: 0.875rem;
+font-style: normal;
+font-weight: 700;
+line-height: normal;
+cursor: pointer;
+}
+
+input {
+width: 20.625rem;
+height: 2.5rem;
+flex-shrink: 0;
+padding: 0 0.625rem;
+margin: 0.625rem 0;
+
+border-radius: 0.313rem;
+border: 0.063rem solid #003BE5;
+background: #FFF;
+
+color: #003BE5;
+font-family: Epilogue;
+font-size: 0.875rem,;
+font-style: normal;
+font-weight: 400;
+line-height: normal;
+}
+
+input::placeholder {
+    color: #003BE5;
+}
+`;
+
+export const LogoContainer = styled.div`
+display: flex;
+align-items: center;
+width: 16.788rem;
+height: 3.509rem;
+`;
+
+export const Logo = styled.div`
+position: relative;
+height: 3.157rem;
+width: 3.157rem;
+margin: 0 0.5rem;
+
+img:nth-of-type(1)  {
+position: absolute;
+width: 3.157rem;
+height: 3.157rem;
+}
+
+img:nth-of-type(2)  {
+position: absolute;
+bottom: 0rem;
+left: 0.9rem;
+width: 2.28rem;
+height: 2.456rem;
+}
+
+img:nth-of-type(3)  {
+position: absolute;
+bottom: 0.1rem;
+right : 0.01rem;
+width: 3.469rem;
+height: 3.469rem;
+}
+`;
+
+export const Name = styled.div`
+display: flex;
+align-items: center;
+
+img:nth-of-type(1) {
+    width: 5.368rem;
+    height: 2.112rem;
+}
+img:nth-of-type(2) {
+    width: 7.01rem;
+    height: 1.672rem;
+}
 `;
